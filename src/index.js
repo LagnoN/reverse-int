bd12e2b4f52ad3bd4b2f32fd68d0622898e12561
Nikolay Lagno <93465053+LagnoN@users.noreply.github.com>
2022-01-09 18:06:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 module.exports = function reverse (n) {
     n = Math.abs(n);
     let x = n.toString(10);
-    let y = x[2] + x[1] + x[0];
+    let y = '';
+    let len = x.length;
+    while (len > 0) {
+        y = y + x[len-1];
+        len--;
+    }
     return parseInt(y, 10);
 }
